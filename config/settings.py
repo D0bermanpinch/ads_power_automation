04509a4e34328404b45e17af7245a89de2bb90f2
D0bermanpinch <makskot2598@gmail.com>
2025-02-13 21:07:42 +0300

    Creating profile 1.0 without working by lines on xlsx 13.02.25

--- config/settings.py
+++ config/settings.py
@@ -3,9 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ADSP_API_URL = "http://local.adspower.net:50325/api/v1"
-ADSP_API_KEY = os.getenv("ADSP_API_KEY")
-PROXY_ID = os.getenv("PROXY_ID")
-BROWSER_VERSION = os.getenv("BROWSER_VERSION")
-OS_TYPE = os.getenv("OS_TYPE")
-WEBRTC_MODE = os.getenv("WEBRTC_MODE")
+class Data_Setup:
+
+    ADSP_API_URL = "http://local.adspower.net:50325/api/v1"
+    ADSP_API_KEY = os.getenv("ADSP_API_KEY")
+    PROXY_ID = os.getenv("PROXY_ID")
+    PROXY_USER = os.getenv("PROXY_USER")
+    PROXY_PASSWORD = os.getenv("PROXY_PASSWORD")
+    PROXY_URL = os.getenv("PROXY_URL")
+    BROWSER_VERSION = os.getenv("BROWSER_VERSION")
+    OS_TYPE = os.getenv("OS_TYPE")
+    WEBRTC_MODE = os.getenv("WEBRTC_MODE")
+    GROUP_ID = os.getenv("GROUP_ID")
--- src/ads_power_api.py
+++ src/ads_power_api.py
@@ -1,13 +1,21 @@
 import requests
 import json
-import os
-from config.settings import ADSP_API_URL, ADSP_API_KEY, PROXY_ID, BROWSER_VERSION, OS_TYPE, WEBRTC_MODE
+from config.settings import Data_Setup
 from src.utils import get_next_account
+
 
 class AdsPowerAPI:
     def __init__(self):
-        self.base_url = ADSP_API_URL
-        self.api_key = ADSP_API_KEY
+        self.base_url = Data_Setup.ADSP_API_URL
+        self.api_key = Data_Setup.ADSP_API_KEY
+        self.proxy_id = Data_Setup.PROXY_ID
+        self.browser_version = Data_Setup.BROWSER_VERSION
+        self.os_type = Data_Setup.OS_TYPE
+        self.webrtc_mode = Data_Setup.WEBRTC_MODE
+        self.group_id = Data_Setup.GROUP_ID
+        self.proxy_user = Data_Setup.PROXY_USER
+        self.proxy_password = Data_Setup.PROXY_PASSWORD
+        self.proxy_url = Data_Setup.PROXY_URL
 
     def get_profiles(self):
         url = f"{self.base_url}/user/list"
@@ -23,33 +31,45 @@ class AdsPowerAPI:
 
         email, password, token = account["Email"], account["Password"], account["Token"]
 
-        cookies = json.dumps([{
-            "domain": ".twitter.com",
-            "httpOnly": False,
-            "path": "/",
-            "secure": False,
-            "expirationDate": 1739009685,
-            "name": "auth_token",
-            "value": token
-        }])
-
         url = f"{self.base_url}/user/create"
         data = {
             "apiKey": self.api_key,
-            "group_id": "Max",
+            "group_id": self.group_id,
             "domain_name": "x.com",
-            "proxy_id": PROXY_ID,
-            "fingerprint_config": {
-                "browser": BROWSER_VERSION,
-                "os": OS_TYPE,
-                "webrtc": WEBRTC_MODE
+            "user_proxy_config": {
+                "proxy_type": "http",
+                "proxy_host": "5.161.22.120",
+                "proxy_port": 13757,
+                "proxy_user": self.proxy_user,
+                "proxy_password": self.proxy_password,
+                "proxy_url": self.proxy_url,
+                "proxy_soft": "other"
             },
-            "cookies": cookies
+            "fingerprint_config": {
+                "browser": self.browser_version,
+                "ua": self.os_type,  # Пробуем передать OS
+                "webrtc": self.webrtc_mode,
+                "canvas": 0,  # Выключаем Canvas
+                "webgl_image": 0  # Выключаем WebGL Image
+            },
+            "cookie": json.dumps([
+                {
+                    "domain": ".twitter.com",
+                    "httpOnly": False,
+                    "path": "/",
+                    "secure": False,
+                    "expirationDate": 1739009685,
+                    "name": "auth_token",
+                    "value": token
+                }]
+            )
         }
+
         response = requests.post(url, json=data)
+        print(response.json())  # Логируем ответ API
         return response.json()
+
 
 if __name__ == "__main__":
     api = AdsPowerAPI()
     new_profile = api.create_profile()
-    print(new_profile)
